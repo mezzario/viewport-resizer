cd86225d5ed0ddaea4248d676fad6997559809ae
Eugene Baranchuk <vencedor@gmail.com>
2024-08-22 15:04:49 -0700

    code optimizations

--- resizer.js
+++ resizer.js
@@ -1,26 +1,22 @@
-const HANDLE_WIDTH = 14;
-const SNAP_HANDLE_DISTANCE = 10;
-const MIN_VIEWPORT_WIDTH = 200;
+const RESIZE_BAR_WIDTH = 14;
+const RESIZE_BAR_SNAP_DISTANCE = 10;
+const MIN_VIEWPORT_WIDTH = 50;
+const GRIP_LINES_COUNT = 2;
 const GRIP_LINE_HEIGHT = 30;
+const GRIP_LINE_WIDTH = 1;
 
 class ViewportResizer {
   constructor() {
-    this.resizeHandle = null;
     this.isResizing = false;
     this.initialX = 0;
     this.initialWidth = 0;
-    this.leftCover = null;
-    this.rightCover = null;
-    this.iframe = null;
     this.currentWidth = this.getStoredWidth();
-    this.overlay = null;
-    this.widthLabel = null;
 
     this.addIframe();
-    this.addResizeHandle();
+    this.addBar();
+    this.addWidthLabel();
     this.addCoverElements();
     this.addOverlay();
-    this.addWidthLabel();
 
     this.addEventListeners();
     this.setupIframeNavigation();
@@ -29,55 +25,57 @@ class ViewportResizer {
 
   addIframe() {
     // Create iframe
-    this.iframe = document.createElement('iframe');
-    this.iframe.style.position = 'fixed';
-    this.iframe.style.top = '0';
-    this.iframe.style.left = '50%';
-    this.iframe.style.transform = 'translateX(-50%)';
-    this.iframe.style.height = '100vh';
-    this.iframe.style.border = 'none';
-    this.iframe.src = window.location.href;
+    this.iframeEl = document.createElement('iframe');
+    this.iframeEl.style.position = 'fixed';
+    this.iframeEl.style.top = '0';
+    this.iframeEl.style.left = '50%';
+    this.iframeEl.style.transform = 'translateX(-50%)';
+    this.iframeEl.style.height = '100vh';
+    this.iframeEl.style.border = 'none';
+    this.iframeEl.src = window.location.href;
 
     // Replace entire HTML content with the iframe
     document.documentElement.innerHTML = '';
-    document.documentElement.appendChild(this.iframe);
+    document.documentElement.appendChild(this.iframeEl);
   }
 
-  addResizeHandle() {
-    this.resizeHandle = document.createElement('div');
-    this.resizeHandle.className = 'viewport-resizer';
-    this.resizeHandle.style.position = 'fixed';
-    this.resizeHandle.style.right = `${
+  addBar() {
+    this.barEl = document.createElement('div');
+    this.barEl.className = 'viewport-resizer';
+    this.barEl.style.position = 'fixed';
+    this.barEl.style.right = `${
       window.innerWidth - (window.innerWidth / 2 + this.currentWidth / 2)
     }px`;
-    this.resizeHandle.style.top = '0';
-    this.resizeHandle.style.width = `${HANDLE_WIDTH}px`;
-    this.resizeHandle.style.height = '100vh';
-    this.resizeHandle.style.cursor = 'ew-resize';
-    this.resizeHandle.style.zIndex = '10000';
-    this.resizeHandle.style.backgroundColor = 'rgba(128, 128, 128, 0.7)';
-    this.resizeHandle.style.margin = '0';
-    this.resizeHandle.style.padding = '0';
-    this.resizeHandle.style.borderRadius = '0';
+    this.barEl.style.top = '0';
+    this.barEl.style.width = `${RESIZE_BAR_WIDTH}px`;
+    this.barEl.style.height = '100vh';
+    this.barEl.style.cursor = 'ew-resize';
+    this.barEl.style.zIndex = '10000';
+    this.barEl.style.backgroundColor = 'rgba(128, 128, 128, 0.7)';
+    this.barEl.style.margin = '0';
+    this.barEl.style.padding = '0';
+    this.barEl.style.borderRadius = '0';
 
-    const lineWidth = 1;
-    const lineGap = 4;
-    const totalLineWidth = lineWidth * 2 + lineGap;
-    for (let i = 0; i < 2; i++) {
+    const totalLineWidth = GRIP_LINE_WIDTH * GRIP_LINES_COUNT;
+    const lineGap =
+      (RESIZE_BAR_WIDTH - totalLineWidth) / (GRIP_LINES_COUNT + 1);
+    const totalWidth = totalLineWidth + lineGap * (GRIP_LINES_COUNT - 1);
+
+    for (let i = 0; i < GRIP_LINES_COUNT; i++) {
       const line = document.createElement('div');
       line.style.position = 'absolute';
       line.style.top = '50%';
       line.style.transform = 'translateY(-50%)';
       line.style.height = `${GRIP_LINE_HEIGHT}px`;
-      line.style.width = `${lineWidth}px`;
+      line.style.width = `${GRIP_LINE_WIDTH}px`;
       line.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
       line.style.left = `${
-        (HANDLE_WIDTH - totalLineWidth) / 2 + i * (lineWidth + lineGap)
+        (RESIZE_BAR_WIDTH - totalWidth) / 2 + i * (GRIP_LINE_WIDTH + lineGap)
       }px`;
-      this.resizeHandle.appendChild(line);
+      this.barEl.appendChild(line);
     }
 
-    document.documentElement.appendChild(this.resizeHandle);
+    document.documentElement.appendChild(this.barEl);
   }
 
   addCoverElements() {
@@ -89,71 +87,71 @@ class ViewportResizer {
       zIndex: '9998',
     };
 
-    this.leftCover = document.createElement('div');
-    Object.assign(this.leftCover.style, coverStyle, {left: '0'});
-    document.documentElement.appendChild(this.leftCover);
+    this.leftCoverEl = document.createElement('div');
+    Object.assign(this.leftCoverEl.style, coverStyle, {left: '0'});
+    document.documentElement.appendChild(this.leftCoverEl);
 
-    this.rightCover = document.createElement('div');
-    Object.assign(this.rightCover.style, coverStyle, {right: '0'});
-    document.documentElement.appendChild(this.rightCover);
+    this.rightCoverEl = document.createElement('div');
+    Object.assign(this.rightCoverEl.style, coverStyle, {right: '0'});
+    document.documentElement.appendChild(this.rightCoverEl);
   }
 
   addOverlay() {
-    this.overlay = document.createElement('div');
-    this.overlay.style.position = 'fixed';
-    this.overlay.style.top = '0';
-    this.overlay.style.left = '50%';
-    this.overlay.style.transform = 'translateX(-50%)';
-    this.overlay.style.height = '100vh';
-    this.overlay.style.display = 'none';
-    this.overlay.style.zIndex = '9999';
-    document.documentElement.appendChild(this.overlay);
+    this.overlayEl = document.createElement('div');
+    this.overlayEl.style.position = 'fixed';
+    this.overlayEl.style.top = '0';
+    this.overlayEl.style.left = '50%';
+    this.overlayEl.style.transform = 'translateX(-50%)';
+    this.overlayEl.style.height = '100vh';
+    this.overlayEl.style.display = 'none';
+    this.overlayEl.style.zIndex = '9999';
+    document.documentElement.appendChild(this.overlayEl);
   }
 
   addWidthLabel() {
-    this.widthLabel = document.createElement('div');
-    this.widthLabel.style.position = 'fixed';
-    this.widthLabel.style.top = '10px';
-    this.widthLabel.style.right = '10px';
-    this.widthLabel.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    this.widthLabel.style.color = '#fff';
-    this.widthLabel.style.padding = '5px 10px';
-    this.widthLabel.style.borderRadius = '5px';
-    this.widthLabel.style.fontSize = '12px';
-    this.widthLabel.style.fontFamily = 'monospace'; // Added monospace font
-    this.widthLabel.style.display = 'none';
-    this.widthLabel.style.zIndex = '10001';
-    document.documentElement.appendChild(this.widthLabel);
+    this.widthLabelEl = document.createElement('div');
+    this.widthLabelEl.style.position = 'fixed';
+    this.widthLabelEl.style.top = '10px';
+    this.widthLabelEl.style.right = '10px';
+    this.widthLabelEl.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    this.widthLabelEl.style.color = '#fff';
+    this.widthLabelEl.style.padding = '5px 10px';
+    this.widthLabelEl.style.borderRadius = '5px';
+    this.widthLabelEl.style.fontSize = '12px';
+    this.widthLabelEl.style.fontFamily = 'monospace';
+    this.widthLabelEl.style.display = 'none';
+    this.widthLabelEl.style.zIndex = '10001';
+    document.documentElement.appendChild(this.widthLabelEl);
   }
 
   addEventListeners() {
-    this.resizeHandle.addEventListener('mousedown', this.startResize);
-    this.resizeHandle.addEventListener('mouseenter', this.showWidthLabel);
-    this.resizeHandle.addEventListener('mouseleave', this.hideWidthLabel);
-    this.resizeHandle.addEventListener('dblclick', this.expandToFullWidth);
+    this.barEl.addEventListener('mousedown', this.handleBarMouseDown);
+    this.barEl.addEventListener('mouseenter', this.handleBarMouseEnter);
+    this.barEl.addEventListener('mouseleave', this.handleBarMouseLeave);
+    this.barEl.addEventListener('dblclick', this.handleBarDoubleClick);
 
-    window.addEventListener('mousemove', this.handleResize);
-    window.addEventListener('mouseup', this.stopResize);
+    window.addEventListener('mousemove', this.handleWindowMouseMove);
+    window.addEventListener('mouseup', this.handleWindowMouseUp);
     window.addEventListener('resize', this.handleWindowResize);
   }
 
   removeEventListeners() {
-    this.resizeHandle.removeEventListener('mousedown', this.startResize);
-    this.resizeHandle.removeEventListener('mouseenter', this.showWidthLabel);
-    this.resizeHandle.removeEventListener('mouseleave', this.hideWidthLabel);
-    this.resizeHandle.removeEventListener('dblclick', this.expandToFullWidth);
+    this.barEl.removeEventListener('mousedown', this.handleBarMouseDown);
+    this.barEl.removeEventListener('mouseenter', this.handleBarMouseEnter);
+    this.barEl.removeEventListener('mouseleave', this.handleBarMouseLeave);
+    this.barEl.removeEventListener('dblclick', this.handleBarDoubleClick);
 
-    window.removeEventListener('mousemove', this.handleResize);
-    window.removeEventListener('mouseup', this.stopResize);
+    window.removeEventListener('mousemove', this.handleWindowMouseMove);
+    window.removeEventListener('mouseup', this.handleWindowMouseUp);
     window.removeEventListener('resize', this.handleWindowResize);
   }
 
   resizeViewport = (width = this.currentWidth) => {
     this.currentWidth = width;
 
-    this.iframe.style.width = `${this.currentWidth}px`;
-    this.overlay.style.width = `${this.currentWidth}px`;
-    this.resizeHandle.style.right = `${
+    this.iframeEl.style.width = `${this.currentWidth}px`;
+    this.overlayEl.style.width = `${this.currentWidth}px`;
+    this.barEl.style.right = `${
       window.innerWidth - (window.innerWidth / 2 + this.currentWidth / 2)
     }px`;
 
@@ -161,71 +159,69 @@ class ViewportResizer {
     this.updateWidthLabel();
   };
 
-  startResize = (e) => {
+  handleBarMouseDown = (e) => {
     this.isResizing = true;
     this.initialX = e.clientX;
     this.initialWidth = this.currentWidth;
-    this.overlay.style.display = 'block';
-    this.showWidthLabel();
+    this.overlayEl.style.display = 'block';
+    this.handleBarMouseEnter();
   };
 
-  handleResize = (e) => {
-    if (this.isResizing) {
-      this.resizeViewport(
-        Math.max(
-          MIN_VIEWPORT_WIDTH,
-          Math.min(
-            this.initialWidth + (e.clientX - this.initialX) * 2,
-            window.innerWidth
-          )
+  handleWindowMouseMove = (e) => {
+    if (!this.isResizing) return;
+
+    this.resizeViewport(
+      Math.max(
+        MIN_VIEWPORT_WIDTH,
+        Math.min(
+          this.initialWidth + (e.clientX - this.initialX) * 2,
+          window.innerWidth
         )
-      );
-    }
+      )
+    );
   };
 
-  stopResize = () => {
+  handleWindowMouseUp = () => {
     this.isResizing = false;
-    this.overlay.style.display = 'none';
-    this.hideWidthLabel();
+    this.overlayEl.style.display = 'none';
+    this.handleBarMouseLeave();
 
-    const resizeHandleRight = parseInt(this.resizeHandle.style.right);
-    if (resizeHandleRight < SNAP_HANDLE_DISTANCE) {
+    const barRight = parseInt(this.barEl.style.right);
+    if (barRight < RESIZE_BAR_SNAP_DISTANCE) {
       this.resizeViewport(window.innerWidth);
     }
     this.saveWidth();
 
-    // Update initial values for the next resize
+    // Update initial values for the next resize.
     this.initialWidth = this.currentWidth;
-    this.initialX = window.innerWidth - resizeHandleRight - HANDLE_WIDTH / 2;
+    this.initialX = window.innerWidth - barRight - RESIZE_BAR_WIDTH / 2;
   };
 
   updateCoverElements = () => {
     const coverWidth = Math.max(0, (window.innerWidth - this.currentWidth) / 2);
-    this.leftCover.style.width = `${coverWidth}px`;
-    this.rightCover.style.width = `${coverWidth}px`;
+    this.leftCoverEl.style.width = `${coverWidth}px`;
+    this.rightCoverEl.style.width = `${coverWidth}px`;
   };
 
   updateWidthLabel = () => {
-    this.widthLabel.textContent = `${Math.round(this.currentWidth)}px`;
-    this.widthLabel.style.right = `${
+    this.widthLabelEl.textContent = `${Math.round(this.currentWidth)}px`;
+    this.widthLabelEl.style.right = `${
       window.innerWidth -
       (window.innerWidth / 2 + this.currentWidth / 2) +
-      HANDLE_WIDTH +
+      RESIZE_BAR_WIDTH +
       10
     }px`;
   };
 
-  showWidthLabel = () => {
-    if (!this.isResizing) {
-      this.widthLabel.style.display = 'block';
-      this.updateWidthLabel();
-    }
+  handleBarMouseEnter = () => {
+    if (this.isResizing) return;
+    this.widthLabelEl.style.display = 'block';
+    this.updateWidthLabel();
   };
 
-  hideWidthLabel = () => {
-    if (!this.isResizing) {
-      this.widthLabel.style.display = 'none';
-    }
+  handleBarMouseLeave = () => {
+    if (this.isResizing) return;
+    this.widthLabelEl.style.display = 'none';
   };
 
   getStorageKey(paramName) {
@@ -253,7 +249,7 @@ class ViewportResizer {
     this.resizeViewport(Math.min(this.getStoredWidth(), window.innerWidth));
   };
 
-  expandToFullWidth = () => {
+  handleBarDoubleClick = () => {
     this.resizeViewport(window.innerWidth);
     this.saveWidth();
   };
@@ -262,25 +258,25 @@ class ViewportResizer {
     this.removeEventListeners();
 
     // Redirect to the current URL of the iframe.
-    window.location.href = this.iframe.contentWindow.location.href;
+    window.location.href = this.iframeEl.contentWindow.location.href;
     window.location.reload();
   }
 
   setupIframeNavigation() {
     // Handle full page loads.
-    this.iframe.addEventListener('load', () => {
+    this.iframeEl.addEventListener('load', () => {
       this.updateWrapperUrl();
     });
 
     // Handle hash changes.
-    this.iframe.contentWindow.addEventListener('hashchange', () => {
+    this.iframeEl.contentWindow.addEventListener('hashchange', () => {
       this.updateWrapperUrl();
     });
   }
 
   updateWrapperUrl() {
     try {
-      const iframeUrl = this.iframe.contentWindow.location.href;
+      const iframeUrl = this.iframeEl.contentWindow.location.href;
       if (window.location.href !== iframeUrl) {
         history.replaceState(null, '', iframeUrl);
       }
